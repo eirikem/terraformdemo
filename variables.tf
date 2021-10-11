3ba9cecac7d4ef4399cac31da8d5c82f0857d982
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-10-11 12:10:36 +0200

    comment out sub and ten

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
 
-variable "tenant_id" {
-  type = string
+# variable "tenant_id" {
+#   type = string
 
-}
+# }
 
 variable "location" {
   type    = string
