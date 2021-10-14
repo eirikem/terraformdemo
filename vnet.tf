2a9494a537d766598cda681e3d8931a2ede10cfd
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-10-14 09:03:00 +0200

    removed vnet

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_network" "tfvnet1" {
-  name                = "tfvnet"
-  address_space       = ["10.2.0.0/22"]
-  location            = var.location
-  resource_group_name = var.rg
+# resource "azurerm_virtual_network" "tfvnet1" {
+#   name                = "tfvnet"
+#   address_space       = ["10.2.0.0/22"]
+#   location            = var.location
+#   resource_group_name = var.rg
 
-#   tags = var.tags
-}
+# #   tags = var.tags
+# }
