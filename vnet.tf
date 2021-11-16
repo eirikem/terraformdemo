9bace301409c21b48994f561799d1e794c91f900
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-11-16 09:38:54 +0000

    adding one more vnet

--- vnet.tf
+++ vnet.tf
@@ -1,16 +1,16 @@
-# resource "azurerm_virtual_network" "techlunchvnet1" {
-#   name                = "techlunchvnet1"
-#   address_space       = ["10.2.0.0/16"]
-#   location            = var.location
-#   resource_group_name = var.rg
+resource "azurerm_virtual_network" "techlunchvnet1" {
+  name                = "techlunchvnet1"
+  address_space       = ["10.2.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg
 
-# }
+}
 
 
-# resource "azurerm_virtual_network" "techlunchvnet2" {
-#   name                = "techlunchvnet2"
-#   address_space       = ["10.16.0.0/16"]
-#   location            = var.location
-#   resource_group_name = var.rg
+resource "azurerm_virtual_network" "techlunchvnet2" {
+  name                = "techlunchvnet2"
+  address_space       = ["10.16.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg
 
-# }
+}
