1d72248d5f09cc35148c5d9b3a3d4c0bee056bd3
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-11-16 09:34:16 +0100

    commented out

--- subnet.tf
+++ subnet.tf
@@ -1,17 +1,17 @@
-resource "azurerm_subnet" "subneta" {
-  name                 = "subneta"
-  resource_group_name  = var.rg
-  virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
-  address_prefixes     = ["10.2.1.0/24"]
-}
+# resource "azurerm_subnet" "subneta" {
+#   name                 = "subneta"
+#   resource_group_name  = var.rg
+#   virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
+#   address_prefixes     = ["10.2.1.0/24"]
+# }
 
 
-resource "azurerm_subnet" "subnetb" {
-  name                 = "subnetb"
-  resource_group_name  = var.rg
-  virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
-  address_prefixes     = ["10.2.254.224/27"]
-}
+# resource "azurerm_subnet" "subnetb" {
+#   name                 = "subnetb"
+#   resource_group_name  = var.rg
+#   virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
+#   address_prefixes     = ["10.2.254.224/27"]
+# }
 
 # resource "azurerm_subnet" "subnetb" {
 #   name                 = "subnetb"
