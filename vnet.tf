538d9f9e53e95405c664438de2ea45cdf3a2d937
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-11-17 08:47:54 +0000

    remove vnet

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,10 @@
-resource "azurerm_virtual_network" "techlunchvnet1" {
-  name                = "techlunchvnet1"
-  address_space       = ["10.128.0.0/16"]
-  location            = var.location
-  resource_group_name = var.rg
+# resource "azurerm_virtual_network" "techlunchvnet1" {
+#   name                = "techlunchvnet1"
+#   address_space       = ["10.128.0.0/16"]
+#   location            = var.location
+#   resource_group_name = var.rg
 
-}
+# }
 
 
 # resource "azurerm_virtual_network" "techlunchvnet2" {
