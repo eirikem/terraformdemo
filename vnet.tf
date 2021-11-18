9a5d033842a3b675e4c078fd8ac59c0e0a946928
Eirik Emanuelsen <eirik.emanuelsen@tietoevry.com>
2021-11-18 07:55:38 +0000

    changes

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,26 @@
-resource "azurerm_virtual_network" "techlunchvnet1" {
-  name                = "techlunchvnet1"
-  address_space       = ["10.128.0.0/16"]
-  location            = var.location
-  resource_group_name = var.rg
+# resource "azurerm_virtual_network" "techlunchvnet1" {
+#   name                = "techlunchvnet1"
+#   address_space       = ["10.128.0.0/16"]
+#   location            = var.location
+#   resource_group_name = var.rg
 
-}
+# }
+
+### SUBNET techlunchvnet1 ###
+
+# resource "azurerm_subnet" "vnet1subneta" {
+#   name                 = "vnet1subneta"
+#   resource_group_name  = var.rg
+#   virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
+#   address_prefixes     = ["10.128.1.0/24"]
+# }
+
+# resource "azurerm_subnet" "vnet1subnetb" {
+#   name                 = "vnet1subnetb"
+#   resource_group_name  = var.rg
+#   virtual_network_name = azurerm_virtual_network.techlunchvnet1.name
+#   address_prefixes     = ["10.128.254.224/27"]
+# }
 
 
 # resource "azurerm_virtual_network" "techlunchvnet2" {
@@ -13,4 +29,13 @@ resource "azurerm_virtual_network" "techlunchvnet1" {
 #   location            = var.location
 #   resource_group_name = var.rg
 
+# }
+
+### SUBNET techlunchvnet2 ###
+
+# resource "azurerm_subnet" "vnet2subneta" {
+#   name                 = "vnet2subneta"
+#   resource_group_name  = var.rg
+#   virtual_network_name = azurerm_virtual_network.techlunchvnet2.name
+#   address_prefixes     = ["10.129.255.224/27"]
 # }
